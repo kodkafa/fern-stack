053ad09951cff9f1c4a9d2e48624ac8d1020a9f1
goker <goker@cebeci.name>
2021-07-27 19:43:10 +0300

    eslint fix

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,20 +7,3 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
-// const functions = require('firebase-functions');
-const admin = require('firebase-admin')
-admin.initializeApp()
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-const processSignUp = require('./signup.hooks')
-const users = require('./users.functions')
-
-// exports.hello = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-exports.processSignUp = processSignUp.hook
-exports.users = users.functions
